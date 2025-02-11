2b125896030e24034b39883866ad983a46abf17c
Fernando <fernandolrramos@gmail.com>
2025-02-11 19:35:03 +0100

    Update App.js
    Updated footer.

--- src/App.js
+++ src/App.js
@@ -148,8 +148,7 @@ function App() {
         padding: "10px 0"
       }}>
         © {new Date().getFullYear()} Dieta Carnívora Brasil. Todos os direitos reservados.
-        //© {new Date().getFullYear()} <a href="https://www.dietacarnivorabrasil.com/" target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none", color: "#FF0000", fontWeight: "bold" }}> Dieta Carnívora Brasil</a>. Todos os direitos reservados.
-          </footer>
+      </footer>
           
     </div>
   );
